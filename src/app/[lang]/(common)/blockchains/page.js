e4077a85c89f20f551da2d170c2f1b95353cd790
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-15 11:22:33 +0800

    Create page.js

---
+++ src/app/[lang]/(common)/blockchains/page.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const BlockchainsPage = () => {
+  return (
+    <main>
+      <Typography variant="h1">Hello Blockchains Page!</Typography>
+    </main>
+  );
+};
+
+export default BlockchainsPage;
