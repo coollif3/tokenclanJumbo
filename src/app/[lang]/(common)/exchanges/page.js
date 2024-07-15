0bb614a429de57f331b10023950f6951905d7f3c
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-15 11:22:36 +0800

    Create page.js

---
+++ src/app/[lang]/(common)/exchanges/page.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const ExchangesPage = () => {
+  return (
+    <main>
+      <Typography variant="h1">Hello Exchanges Page!</Typography>
+    </main>
+  );
+};
+
+export default ExchangesPage;
