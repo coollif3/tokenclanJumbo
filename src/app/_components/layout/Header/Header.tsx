1eb3df572e72ad567af76f9520228dd633b04398
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-15 11:39:16 +0800

    Hide message block in header

--- src/app/_components/layout/Header/Header.tsx
+++ src/app/_components/layout/Header/Header.tsx
@@ -1,19 +1,19 @@
-'use client';
-import { AuthUserPopover } from '@app/_components/popovers/AuthUserPopover';
-import { MessagesPopover } from '@app/_components/popovers/MessagesPopover';
-import { NotificationsPopover } from '@app/_components/popovers/NotificationsPopover';
-import { useSidebarState } from '@jumbo/components/JumboLayout/hooks';
-import { useJumboHeaderTheme } from '@jumbo/components/JumboTheme/hooks';
+"use client";
+import { AuthUserPopover } from "@app/_components/popovers/AuthUserPopover";
+import { MessagesPopover } from "@app/_components/popovers/MessagesPopover";
+import { NotificationsPopover } from "@app/_components/popovers/NotificationsPopover";
+import { useSidebarState } from "@jumbo/components/JumboLayout/hooks";
+import { useJumboHeaderTheme } from "@jumbo/components/JumboTheme/hooks";
 
-import { SIDEBAR_STYLES } from '@jumbo/utilities/constants';
+import { SIDEBAR_STYLES } from "@jumbo/utilities/constants";
 
-import { Logo } from '@app/_components/_core/Logo';
-import { SidebarToggleButton } from '@app/_components/_core/SidebarToggleButton';
-import { TranslationPopover } from '@app/_components/popovers/TranslationPopover';
-import { Stack } from '@mui/material';
-import React from 'react';
-import { Search, SearchIconButtonOnSmallScreen } from './components';
-import { ThemeModeOption } from './components/ThemeModeOptions';
+import { Logo } from "@app/_components/_core/Logo";
+import { SidebarToggleButton } from "@app/_components/_core/SidebarToggleButton";
+import { TranslationPopover } from "@app/_components/popovers/TranslationPopover";
+import { Stack } from "@mui/material";
+import React from "react";
+import { Search, SearchIconButtonOnSmallScreen } from "./components";
+import { ThemeModeOption } from "./components/ThemeModeOptions";
 
 function Header() {
   const { isSidebarStyle } = useSidebarState();
@@ -29,14 +29,14 @@ function Header() {
     <React.Fragment>
       <SidebarToggleButton />
       {isSidebarStyle(SIDEBAR_STYLES.CLIPPED_UNDER_HEADER) && (
-        <Logo sx={{ mr: 3 }} mode={headerTheme.type ?? 'light'} />
+        <Logo sx={{ mr: 3 }} mode={headerTheme.type ?? "light"} />
       )}
       <Search show={searchVisibility} onClose={handleSearchVisibility} />
-      <Stack direction='row' alignItems='center' gap={1.25} sx={{ ml: 'auto' }}>
+      <Stack direction="row" alignItems="center" gap={1.25} sx={{ ml: "auto" }}>
         <ThemeModeOption />
         <TranslationPopover />
         <SearchIconButtonOnSmallScreen onClick={handleSearchVisibility} />
-        <MessagesPopover />
+        {/* <MessagesPopover /> */}
         <NotificationsPopover />
         <AuthUserPopover />
       </Stack>
