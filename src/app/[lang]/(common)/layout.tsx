663391cd380edccb15a24edcb454d0e31eb1d8c0
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-18 16:35:53 +0800

    Revert "Merge branch 'mainLayout' into db"
    This reverts commit a1f8c57fb3a8fa4dc3a0c475750c6d3a3e1c767e, reversing
    changes made to 7e3d014b6faebdff72db9729ae397d4046dda666.

--- src/app/[lang]/(common)/layout.tsx
+++ src/app/[lang]/(common)/layout.tsx
@@ -1,5 +1,5 @@
 import { Footer, Header, Sidebar } from '@app/_components/layout';
-import { clippedUnderHeaderLayoutConfig } from '@app/_config/layouts/layout2';
+import { defaultLayoutConfig } from '@app/_config/layouts';
 import { getMenus } from '@app/_services';
 import { JumboLayout, JumboLayoutProvider } from '@jumbo/components';
 import { MenuItems } from '@jumbo/types';
@@ -10,13 +10,13 @@ interface CommonLayoutProps {
   params: { lang: string };
 }
 
-export default async function ClippedUnderHeaderLayout({
+export default async function CommonLayout({
   children,
   params: { lang },
 }: CommonLayoutProps) {
   const menus: MenuItems = await getMenus(lang);
   return (
-    <JumboLayoutProvider layoutConfig={clippedUnderHeaderLayoutConfig}>
+    <JumboLayoutProvider layoutConfig={defaultLayoutConfig}>
       <JumboLayout
         header={<Header />}
         footer={<Footer lang={lang} />}
--- src/app/_components/layout/Header/Header.tsx
+++ src/app/_components/layout/Header/Header.tsx
@@ -2,7 +2,6 @@
 import { AuthUserPopover } from "@app/_components/popovers/AuthUserPopover";
 import { MessagesPopover } from "@app/_components/popovers/MessagesPopover";
 import { NotificationsPopover } from "@app/_components/popovers/NotificationsPopover";
-import { DropDownPopover } from "@app/_components/popovers/DropDownPopover";
 import { useSidebarState } from "@jumbo/components/JumboLayout/hooks";
 import { useJumboHeaderTheme } from "@jumbo/components/JumboTheme/hooks";
 
@@ -11,7 +10,7 @@ import { SIDEBAR_STYLES } from "@jumbo/utilities/constants";
 import { Logo } from "@app/_components/_core/Logo";
 import { SidebarToggleButton } from "@app/_components/_core/SidebarToggleButton";
 import { TranslationPopover } from "@app/_components/popovers/TranslationPopover";
-import { Stack, Link } from "@mui/material";
+import { Stack } from "@mui/material";
 import React from "react";
 import { Search, SearchIconButtonOnSmallScreen } from "./components";
 import { ThemeModeOption } from "./components/ThemeModeOptions";
@@ -30,19 +29,16 @@ function Header() {
     <React.Fragment>
       <SidebarToggleButton />
       {isSidebarStyle(SIDEBAR_STYLES.CLIPPED_UNDER_HEADER) && (
-        <Link href="/" sx={{ my: 0 }} color="inherit" underline="none">
-          <Logo sx={{ mr: 3 }} mode={headerTheme.type ?? "light"} />
-        </Link>
+        <Logo sx={{ mr: 3 }} mode={headerTheme.type ?? "light"} />
       )}
-      {/* <Search show={searchVisibility} onClose={handleSearchVisibility} /> */}
+      <Search show={searchVisibility} onClose={handleSearchVisibility} />
       <Stack direction="row" alignItems="center" gap={1.25} sx={{ ml: "auto" }}>
-        {/* {/* <ThemeModeOption /> */}
-        {/* <TranslationPopover /> */}
-        {/* <SearchIconButtonOnSmallScreen onClick={handleSearchVisibility} /> */}
+        <ThemeModeOption />
+        <TranslationPopover />
+        <SearchIconButtonOnSmallScreen onClick={handleSearchVisibility} />
         {/* <MessagesPopover /> */}
-        {/* <NotificationsPopover /> */}
-        <DropDownPopover />
-        {/* <AuthUserPopover /> */}
+        <NotificationsPopover />
+        <AuthUserPopover />
       </Stack>
     </React.Fragment>
   );
