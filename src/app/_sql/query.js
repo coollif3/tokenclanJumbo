92f9b6bfd57bd1e4647669b7a17e1a63c8212a45
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-18 17:26:00 +0800

    Update query.js

--- src/app/_sql/query.js
+++ src/app/_sql/query.js
@@ -1,4 +1,4 @@
 export default {
   listAllBlockchains:
-    "SELECT blockchain_id, name, usd, dominance, createdAt, 1day_usd_chng, 7day_usd_chng, 30day_usd_chng FROM `blockchain_tvl_chng` WHERE id IN (SELECT MAX(id) FROM `blockchain_tvl_chng` GROUP BY blockchain_id) ORDER BY `blockchain_tvl_chng`.`dominance` DESC;",
+    "SELECT blockchain_id, slug, name, usd, dominance, createdAt, 1day_usd_chng, 7day_usd_chng, 30day_usd_chng FROM `blockchain_tvl_chng` WHERE id IN (SELECT MAX(id) FROM `blockchain_tvl_chng` GROUP BY blockchain_id) ORDER BY `blockchain_tvl_chng`.`dominance` DESC;",
 };
