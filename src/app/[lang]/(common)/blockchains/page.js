7cd40a015a75141774621b2e1bb7ecc093c271dc
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-18 17:08:36 +0800

    Added first blockchain call table

--- src/app/[lang]/(common)/blockchains/page.js
+++ src/app/[lang]/(common)/blockchains/page.js
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material";
+import DataTable from "../../../_components/widgets/BlockchainTableListing/DataTable";
+import { listBlockchains } from "../../../_services/blockchain";
 
-const BlockchainsPage = () => {
+const BlockchainsPage = async () => {
+  const listingRows = await listBlockchains();
   return (
     <main>
-      <Typography variant="h1">Hello Blockchains Page!</Typography>
+      <Typography variant="h1" sx={{ mb: 2 }}>
+        Hello Blockchains Page!
+      </Typography>
+      <DataTable rows={listingRows} />
     </main>
   );
 };
--- src/app/api/blockchain/route.js
+++ src/app/api/blockchain/route.js
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 import { blockchain as db } from "../../_config/db/db";
+import query from "../../_sql/query";
 
 export async function GET(req, { params }) {
   try {
-    const [results, metadata] = await db.query(
-      "SELECT bc.id, bc.name AS blockchain, bc.slug, bc.coin_id, c.symbol AS gas_coin, bc.createdAt FROM blockchains AS bc INNER JOIN coins AS c ON bc.coin_id = c.id; "
-    );
+    const [results, metadata] = await db.query(query.listAllBlockchains);
     return NextResponse.json(results, { status: 200 });
   } catch (error) {
     console.log(error);
