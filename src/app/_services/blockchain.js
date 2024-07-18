dbc84a62d56dfabfb15bc4bfc5c205b9121d76b5
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-18 18:35:38 +0800

    Add revalidate

--- src/app/_services/blockchain.js
+++ src/app/_services/blockchain.js
@@ -3,18 +3,20 @@
 import { cache } from "react";
 import { unstable_cache as nextCache } from "next/cache";
 import { blockchain as db } from "../_config/db/db";
-import query from "../_sql/query";
+import { listAllBlockchains } from "../_sql/query";
 
 const listBlockchains = nextCache(
   cache(async () => {
     try {
-      const [results, metadata] = await db.query(query.listAllBlockchains);
+      const [results, metadata] = await db.query(listAllBlockchains);
       return results;
     } catch (error) {
       console.log(error);
       throw new Error("Error fetching all blockchain data");
     }
-  })
+  }),
+  ["listBlockchains"],
+  { revalidate: 28800 }
 );
 
 export { listBlockchains };
