28febe28012cc0b555ac17d2735ab87b2cb4caf5
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-18 20:08:46 +0800

    Update page.js

--- src/app/[lang]/(common)/exchanges/page.js
+++ src/app/[lang]/(common)/exchanges/page.js
@@ -1,9 +1,13 @@
 import { Typography } from "@mui/material";
+import { listExchanges } from "../../../_services/exchanges";
+import ExchangeDataTable from "../../../_components/widgets/ExchangeTableListing/ExchangeDataTable";
 
-const ExchangesPage = () => {
+const ExchangesPage = async () => {
+  const results = await listExchanges();
   return (
     <main>
       <Typography variant="h1">Hello Exchanges Page!</Typography>
+      <ExchangeDataTable rows={results} />
     </main>
   );
 };
