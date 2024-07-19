720169c6a41065195866cfb4f1e57bddc0829fe1
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-19 16:42:13 +0800

    Update blockchain.js

--- src/app/_services/blockchain.js
+++ src/app/_services/blockchain.js
@@ -3,9 +3,9 @@
 import { cache } from "react";
 import { unstable_cache as nextCache } from "next/cache";
 import { blockchain as db } from "../_config/db/db";
-import { listAllBlockchains } from "../_sql/query";
+import { listAllBlockchains, getDefiMktOverview } from "../_sql/query";
 
-const listBlockchains = nextCache(
+export const listBlockchains = nextCache(
   cache(async () => {
     try {
       const [results, metadata] = await db.query(listAllBlockchains);
@@ -19,4 +19,16 @@ const listBlockchains = nextCache(
   { revalidate: 28800 }
 );
 
-export { listBlockchains };
+export const blockchainMktOverview = nextCache(
+  cache(async () => {
+    try {
+      const [results, metadata] = await db.query(getDefiMktOverview);
+      return results;
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error fetching blockchain market overview data");
+    }
+  }),
+  ["getBlockchainOverview"],
+  { revalidate: 28800 }
+);
