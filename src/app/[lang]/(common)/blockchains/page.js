2d791d66f411faae33a263d9aa9b2acac7dff794
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-23 12:33:17 +0800

    First global chart display

--- src/app/[lang]/(common)/blockchains/page.js
+++ src/app/[lang]/(common)/blockchains/page.js
@@ -1,16 +1,39 @@
-import { Typography } from "@mui/material";
 import DataTable from "../../../_components/widgets/BlockchainTableListing/DataTable";
-import { listBlockchains } from "../../../_services/blockchain";
+import {
+  blockchainMktOverview,
+  listBlockchains,
+} from "../../../_services/blockchain";
+import { Container, Grid } from "@mui/material";
+import { CONTAINER_MAX_WIDTH } from "@app/_config/layouts";
+import GlobalCharts from "../../../_components/charts/apex/GlobalCharts";
+import { formatToTimestampArray } from "@app/_utilities/helpers";
 
 const BlockchainsPage = async () => {
   const listingRows = await listBlockchains();
+  const chartSeries = await blockchainMktOverview();
+  const formattedSeries = formatToTimestampArray(chartSeries);
+
   return (
-    <main>
-      <Typography variant="h1" sx={{ mb: 2 }}>
-        Hello Blockchains Page!
-      </Typography>
-      <DataTable rows={listingRows} />
-    </main>
+    <Container
+      maxWidth={false}
+      sx={{
+        maxWidth: CONTAINER_MAX_WIDTH,
+        display: "flex",
+        minWidth: 0,
+        flex: 1,
+        flexDirection: "column",
+      }}
+      disableGutters
+    >
+      <Grid container spacing={3.75}>
+        <Grid item xs={12}>
+          <GlobalCharts series={formattedSeries} />
+        </Grid>
+        <Grid item xs={12}>
+          <DataTable rows={listingRows} />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
