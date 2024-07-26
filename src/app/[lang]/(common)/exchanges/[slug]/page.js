7175149fde616193ba107c4f01821af3cc69209b
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-26 23:42:25 +0800

    Update page.js

--- src/app/[lang]/(common)/exchanges/[slug]/page.js
+++ src/app/[lang]/(common)/exchanges/[slug]/page.js
@@ -9,7 +9,7 @@ import {
   getExchangeTvevChngFor,
 } from "@app/_services/exchange";
 import ExchangeCharts from "@app/_components/charts/apex/ExchangeCharts";
-import PercentChngCard from "../../../../_components/metrics/PercentChngCard/PercentChngCard";
+import PercentChngCard from "@app/_components/metrics/PercentChngCard/PercentChngCard";
 import CurrentMarketCard from "@app/_components/widgets/CurrentMarketCard/CurrentMarketCard";
 
 const volumeChartConfig = {
