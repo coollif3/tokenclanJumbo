3d2a92cc8ea269485ae15d9a1280d8b152b26e38
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-26 15:56:24 +0800

    Create page.js

---
+++ src/app/[lang]/(common)/exchanges/[slug]/page.js
@@ -0,0 +1,78 @@
+import { Container, Grid, Typography } from "@mui/material";
+import { CONTAINER_MAX_WIDTH } from "@app/_config/layouts";
+import {
+  listExchangeVolumeFor,
+  getExchangeVolumeChngFor,
+} from "@app/_services/exchanges";
+import ExchangeCharts from "@app/_components/charts/apex/ExchangeCharts";
+import PercentChngCard from "../../../../_components/metrics/PercentChngCard/PercentChngCard";
+
+const chartConfig = {
+  chartTitle: "Exchange Volume USD",
+  tooltipSeries: "Volume",
+  yaxisTitle: "24hr Volume USD (Billions)",
+};
+
+export default async function ExchangeDetailedPage({ params }) {
+  const slug = params.slug;
+  const volData = await listExchangeVolumeFor(slug, 30);
+  const volChng = await getExchangeVolumeChngFor(slug);
+  return (
+    <>
+      <Container
+        maxWidth={false}
+        sx={{
+          maxWidth: CONTAINER_MAX_WIDTH,
+          display: "flex",
+          minWidth: 0,
+          flex: 1,
+          flexDirection: "column",
+        }}
+        disableGutters
+      >
+        <Grid container spacing={3.75}>
+          <Grid item xs={12}>
+            <Typography variant="h2">Exchange Volume USD</Typography>
+          </Grid>
+
+          <Grid item xs={3}>
+            <Typography variant="h2">Current</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <PercentChngCard
+              title={`24hr Change`}
+              value={parseFloat(volChng.one_day_chng)}
+              period={"day"}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <PercentChngCard
+              title={`7 Day Change`}
+              value={parseFloat(volChng.seven_day_chng)}
+              period={"week"}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <PercentChngCard
+              title={`30 Day Change`}
+              value={parseFloat(volChng.thirty_day_chng)}
+              period={"month"}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <ExchangeCharts series={volData} config={chartConfig} />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography variant="h2">Market Cap (USD)</Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography variant="h2">TVEV Ratio</Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
