13913989e32fb24b6e21f26a0ef030516e379a87
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-26 17:30:35 +0800

    Update query.js

--- src/app/_sql/query.js
+++ src/app/_sql/query.js
@@ -18,10 +18,10 @@ export const getExchangeVolumeChngBySlug =
   "SELECT v.vol_24hr_normalized, v.1day_vol_norm_chng AS one_day_chng, v.7day_vol_norm_chng AS seven_day_chng, v.30day_vol_norm_chng AS thirty_day_chng FROM volume_norm_chng AS v INNER JOIN exchanges AS e ON e.id = v.exchange_id INNER JOIN coins AS c ON c.id = e.coin_id WHERE c.name_id = :slug ORDER BY v.created_on DESC LIMIT 1;";
 
 export const getExchangeMktcapChngBySlug =
-  "SELECT m.market_cap, m.1day_mktcap_chng, m.7day_mktcap_chng, m.30day_mktcap_chng FROM mktcap_usd_chng AS m INNER JOIN coins AS c ON c.id = m.coin_id WHERE c.name_id = :slug ORDER BY m.created_on DESC LIMIT 1;";
+  "SELECT m.market_cap, m.1day_mktcap_chng AS one_day_chng, m.7day_mktcap_chng AS seven_day_chng, m.30day_mktcap_chng AS thirty_day_chng FROM mktcap_usd_chng AS m INNER JOIN coins AS c ON c.id = m.coin_id WHERE c.name_id = :slug ORDER BY m.created_on DESC LIMIT 1;";
 
 export const getExchangeTvevBySlug =
   "SELECT t.created_on AS x, t.ratio AS y FROM `coin_tvev` AS t INNER JOIN coins AS c ON t.coin_id = c.id WHERE c.name_id = :slug  ORDER BY t.`created_on` DESC LIMIT :periodLimit";
 
 export const getExchangeTvevChngBySlug =
-  "SELECT t.ratio, t.1day_ratio_chng, t.7day_ratio_chng, t.30day_ratio_chng FROM coin_tvev_chng AS t INNER JOIN coins AS c ON c.id = t.coin_id WHERE c.name_id = :slug ORDER BY t.created_on DESC LIMIT 1";
+  "SELECT t.ratio, t.1day_ratio_chng AS one_day_chng, t.7day_ratio_chng AS seven_day_chng, t.30day_ratio_chng AS thirty_day_chng FROM coin_tvev_chng AS t INNER JOIN coins AS c ON c.id = t.coin_id WHERE c.name_id = :slug ORDER BY t.created_on DESC LIMIT 1";
