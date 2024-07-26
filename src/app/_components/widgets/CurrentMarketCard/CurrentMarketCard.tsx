9a127387b4b1d463715cc4e9dc187c9f6b42d694
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-26 17:09:51 +0800

    Create CurrentMarketCard.tsx

---
+++ src/app/_components/widgets/CurrentMarketCard/CurrentMarketCard.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { ASSET_IMAGES } from "@app/_utilities/constants/paths";
+import { JumboCard } from "@jumbo/components";
+import { Div } from "@jumbo/shared";
+import { Typography } from "@mui/material";
+import Image from "next/image";
+import { numberWithCommas } from "@app/_utilities/helpers";
+
+const CurrentMarketCard = ({
+  subheader,
+  value,
+}: {
+  subheader: React.ReactNode;
+  value: string;
+}) => {
+  const marketValue = (+value).toFixed();
+  const formattedValue = numberWithCommas(marketValue);
+  return (
+    <JumboCard
+      bgcolor={["#23BCBA"]}
+      contentSx={{ p: 3, height: 145 }}
+      contentWrapper
+    >
+      <Div sx={{ display: "flex", alignItems: "center" }}>
+        <Image
+          alt={""}
+          width={48}
+          height={48}
+          src={`${ASSET_IMAGES}/dashboard/filesIcon.svg`}
+        />
+        <Div sx={{ ml: 2, flex: 1 }}>
+          <Typography color={"common.white"} variant={"h2"} mb={0.5}>
+            {`$ ${formattedValue} `}
+          </Typography>
+          <Typography color={"common.white"} variant={"h5"} mb={0}>
+            {subheader}
+          </Typography>
+        </Div>
+      </Div>
+    </JumboCard>
+  );
+};
+
+export default CurrentMarketCard;
