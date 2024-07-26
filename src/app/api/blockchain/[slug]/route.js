ffc9ae840758c76b0a4bf03a1a04324f87f93973
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-26 23:48:52 +0800

    Create route.js

---
+++ src/app/api/blockchain/[slug]/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import {
+  getBlockchainTvlForSlug,
+  getBlockchainTvlChngForSlug,
+} from "@app/_services/blockchain";
+
+export async function GET(req, { params, query }) {
+  try {
+    const slug = params.slug;
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.searchParams);
+    const period = searchParams.get("period");
+    // console.log("period: ", period);
+
+    const tvlData = await getBlockchainTvlForSlug(slug, parseInt(period));
+    const tvlChngData = await getBlockchainTvlChngForSlug(slug);
+
+    return NextResponse.json(
+      {
+        results: {
+          tvlData: tvlData,
+          tvlChng: tvlChngData,
+          //   marketcap: marketcapData,
+          //   marketcapChng,
+          //   tvev: tvevData,
+          //   tvevChng,
+        },
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        error: "Server error in retrieving blockchain slug data.",
+      },
+      { status: 500 }
+    );
+  }
+}
