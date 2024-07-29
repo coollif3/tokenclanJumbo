7704e2a25505bcc0826713e9468451b76e468bb3
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-07-29 11:04:14 +0800

    Added google analytic tag

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -1,28 +1,30 @@
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
- 
-import { CONFIG } from '@app/_config';
-import '@app/_themes/assets/fonts/noir-pro/styles.css';
-import '@app/_utilities/style/style.css';
-import { JumboConfigProvider, JumboTheme } from '@jumbo/components';
-import { CssBaseline } from '@mui/material';
-import Link from 'next/link';
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+
+import { CONFIG } from "@app/_config";
+import "@app/_themes/assets/fonts/noir-pro/styles.css";
+import "@app/_utilities/style/style.css";
+import { JumboConfigProvider, JumboTheme } from "@jumbo/components";
+import { CssBaseline } from "@mui/material";
+import Link from "next/link";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export async function generateStaticParams() {
-  return [{ lang: 'en-US' }];
+  return [{ lang: "en-US" }];
 }
 export const metadata = {
-  title: 'Jumbo - Admin Dashboard',
-  icons: '/assets/images/favicon.ico',
+  title: "Jumbo - Admin Dashboard",
+  icons: "/assets/images/favicon.ico",
 };
 
 export default async function RootLayout({ children, params: { lang } }) {
-  return ( 
+  return (
     <html lang={lang}>
       <body>
-        <div id='root'>
+        <div id="root">
           <AppRouterCacheProvider>
             <JumboConfigProvider LinkComponent={Link}>
               <JumboTheme init={CONFIG.THEME}>
+                <GoogleAnalytics gaId={process.env.GA_ANALYTICS} />
                 <CssBaseline />
                 {children}
               </JumboTheme>
@@ -30,6 +32,6 @@ export default async function RootLayout({ children, params: { lang } }) {
           </AppRouterCacheProvider>
         </div>
       </body>
-    </html> 
+    </html>
   );
 }
