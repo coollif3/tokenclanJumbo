d64b3674d5de7c353cfecddc2a655ae4e063f4c3
charlieng <charliengcl@outlook.com>
2024-08-15 16:02:50 +0800

    edited and added in new carousel components

---
+++ src/app/_components/_core/CarouselImg/CarouselImg.jsx
@@ -0,0 +1,50 @@
+'use client';
+import Carousel from 'react-material-ui-carousel';
+import {
+  Typography,Paper,Button,CardMedia,Grid
+} from "@mui/material";
+import { Div } from "@jumbo/shared";
+
+import { getAssetPath } from "@app/_utilities/helpers";
+
+
+function CarouselImg({items}) {
+  return (
+    <Carousel>
+      {items.map((item,index) => 
+        <Item item={item} key={index} />
+      )}
+    </Carousel>
+  );
+}
+
+function Item(props) {
+  return (
+    <Paper elevation={0}>
+      <Grid container alignItems="center" justifyContent="center">
+        <Grid item xs={12} md={6}>
+          <Div
+              sx={{ position: "relative", height: 0, paddingBottom: "69.5%" }}
+            >
+              <CardMedia
+                component="img"
+                image={getAssetPath(
+                  `${props.item.image}`,
+                  "640x640"
+                )}
+                alt=""
+                sx={{
+                  inset: 0,
+                  height: "100%",
+                  position: "absolute",
+                  borderRadius: 2,
+                }}
+              />
+            </Div>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+
+export {CarouselImg};
---
+++ src/app/_components/_core/CarouselMain/CarouselMain.jsx
@@ -0,0 +1,70 @@
+'use client';
+import Carousel from 'react-material-ui-carousel';
+import {
+  Typography,Paper,Button,CardMedia,Grid
+} from "@mui/material";
+import { Div } from "@jumbo/shared";
+
+import { getAssetPath } from "@app/_utilities/helpers";
+
+
+function CarouselMain({items}) {
+  return (
+    <Carousel>
+      {items.map((item,index) => 
+        <Item item={item} key={index} />
+      )}
+    </Carousel>
+  );
+}
+
+function Item(props) {
+  return (
+    <Paper elevation={0} sx={{ borderRadius: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6} order={{ md: props.item.imagePosition === 'left' ? 1 : 2 }}>
+          <Div
+              sx={{ position: "relative", height: 0, paddingBottom: "69.5%" }}
+            >
+              <CardMedia
+                component="img"
+                image={getAssetPath(
+                  `${props.item.image}`,
+                  "640x640"
+                )}
+                alt=""
+                sx={{
+                  inset: 0,
+                  height: "100%",
+                  position: "absolute",
+                  borderRadius: 2,
+                }}
+              />
+            </Div>
+        </Grid>
+        <Grid
+          item xs={12} md={6}
+          order={{ md: props.item.imagePosition === 'left' ? 2 : 1 }}
+          sx={{
+            alignSelf: "center",
+            textAlign: props.item.imagePosition === 'right' ? { sm: "left", md: "right" } : '',
+          }}
+        >
+            <Typography variant="h3">{props.item.title}</Typography>
+            <Typography variant="body1" mb={5}>
+              {props.item.description}
+            </Typography>
+            <Button
+              variant={"contained"}
+              href={props.item.buttonLink}
+              sx={{ bgcolor: "#2D5FB4", ":hover": { bgcolor: "#5DCBA6" }}}
+            >
+              {props.item.buttonName}
+            </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+
+export {CarouselMain};
