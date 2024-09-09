6f4588ebe879f30f04a36857a32af8378b404413
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-09-09 16:22:22 +0800

    exchange slug tvev page

---
+++ src/app/[lang]/(common)/exchanges/[slug]/tvev/page.js
@@ -0,0 +1,114 @@
+import { Container, Grid, Typography, Breadcrumbs, Link } from "@mui/material";
+import { CONTAINER_MAX_WIDTH } from "@app/_config/layouts";
+import {
+  getExchangeTvevFor,
+  getExchangeTvevChngFor,
+  getExchangeNameFor,
+  getExchanges,
+} from "@app/_services/exchange";
+import GlobalCharts from "@app/_components/charts/apex/GlobalCharts";
+import PercentChngCard from "@app/_components/metrics/PercentChngCard/PercentChngCard";
+import CurrentMarketCard from "@app/_components/widgets/CurrentMarketCard/CurrentMarketCard";
+
+export async function generateMetadata({ params, searchParams }) {
+  const slug = params.slug;
+  const exchange = await getExchangeNameFor(slug);
+
+  return {
+    title: `${exchange.name} Exchange TVEV Ratio | TokenClan`,
+    description: `${exchange.name} Exchange Volume to Marketcap Ratio history on chart.`,
+  };
+}
+
+export async function generateStaticParams() {
+  const rows = await getExchanges();
+  return rows.map((row) => ({ slug: row.slug }));
+}
+
+const mktcapChartConfig = {
+  chartTitle: "Exchange TVEV Ratio",
+  tooltipSeries: "Ratio",
+  yaxisTitle: "Ratio",
+  yaxisFormatter: "THOUSAND_SEPARATOR",
+  yaxisTooltipFormatterLabel: "RATIO",
+};
+
+export default async function SlugMktcapPage({ params }) {
+  const slug = params.slug;
+
+  const exchange = await getExchangeNameFor(slug);
+  const tvevData = await getExchangeTvevFor(slug, 30);
+  const tvevChng = await getExchangeTvevChngFor(slug);
+
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        maxWidth: CONTAINER_MAX_WIDTH,
+        display: "flex",
+        minWidth: 0,
+        flex: 1,
+        flexDirection: "column",
+      }}
+      disableGutters
+    >
+      <Grid container spacing={3.75} sx={{ my: 3 }}>
+        <Grid item xs={12} sm={4}>
+          <Typography variant="h3">{`${exchange.name} TVEV`}</Typography>
+        </Grid>
+        <Grid item xs={12} sm={4} sx={{ marginLeft: "auto" }}>
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/">
+              Home
+            </Link>
+            <Link underline="hover" color="inherit" href="/exchanges">
+              Exchanges
+            </Link>
+            <Link underline="hover" color="inherit" href={`/exchanges/${slug}`}>
+              {exchange.name}
+            </Link>
+            <Typography color="text.primary">TVEV</Typography>
+          </Breadcrumbs>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={3.75}>
+        <Grid item xs={12} sm={6} md={3}>
+          <CurrentMarketCard
+            subheader={"Today's Ratio"}
+            value={tvevChng.ratio}
+            prefixUnit={""}
+            roundedDigit={0}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PercentChngCard
+            title={`24hr Change`}
+            value={parseFloat(tvevChng.one_day_chng)}
+            period={"day"}
+            unit={"%"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PercentChngCard
+            title={`7 Day Change`}
+            value={parseFloat(tvevChng.seven_day_chng)}
+            period={"week"}
+            unit={"%"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PercentChngCard
+            title={`30 Day Change`}
+            value={parseFloat(tvevChng.thirty_day_chng)}
+            period={"month"}
+            unit={"%"}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <GlobalCharts series={tvevData} config={mktcapChartConfig} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
