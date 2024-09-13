6378f6cc793f9494f75040144a30233d952ac5bc
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-09-13 10:15:59 +0800

    Update JumboNavbarProvider.jsx
    replace lorem ipsum codes

--- src/@jumbo/components/JumboNavbar/components/JumboNavbarProvider/JumboNavbarProvider.jsx
+++ src/@jumbo/components/JumboNavbar/components/JumboNavbarProvider/JumboNavbarProvider.jsx
@@ -1,19 +1,18 @@
-import { useJumboTheme } from '@jumbo/components/JumboTheme/hooks';
-import { isNavSection } from '@jumbo/utilities/helpers';
-import { Box, Button, List, Modal } from '@mui/material';
-import PropTypes from 'prop-types';
-import { JumboNavIdentifier } from '..';
-import { JumboNavbarContext } from './JumboNavbarContext';
-import { JumboCard } from '@jumbo/components/JumboCard';
-import React from 'react';
-import { NewsLetterSubscription } from '@app/_components/widgets/NewsLetterSubscription';
-
+import { useJumboTheme } from "@jumbo/components/JumboTheme/hooks";
+import { isNavSection } from "@jumbo/utilities/helpers";
+import { Box, Button, List, Modal } from "@mui/material";
+import PropTypes from "prop-types";
+import { JumboNavIdentifier } from "..";
+import { JumboNavbarContext } from "./JumboNavbarContext";
+import { JumboCard } from "@jumbo/components/JumboCard";
+import React from "react";
+import { NewsLetterSubscription } from "@app/_components/widgets/NewsLetterSubscription";
 
 function JumboNavbarProvider({
   items = {},
   mini = false,
   open = true,
-  groupBehaviour = 'collapsible',
+  groupBehaviour = "collapsible",
   theme,
 }) {
   const miniAndClosed = !!mini && !open;
@@ -29,11 +28,11 @@ function JumboNavbarProvider({
   const [isOpen, setIsOpen] = React.useState(false);
 
   const handleOpen = () => {
-    setIsOpen(true)
-  }
+    setIsOpen(true);
+  };
   const handleClose = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
 
   let isFirstSection = true;
   return (
@@ -59,24 +58,43 @@ function JumboNavbarProvider({
           return <JumboNavIdentifier item={item} key={index} />;
         })}
       </List>
-      <Box sx={{ left:'50%',
-              bottom:{xs:"-5%",sm:'10px',md:'10px',lg:'10px',xl:'10px'},
-              transform:'translate(-50%)',
-              width: "100%",
-              position:{xs:'relative',sm: 'absolute',
-              md: 'absolute',
-              lg: 'absolute',
-              xl: 'absolute',}}}>
+      <Box
+        sx={{
+          left: "50%",
+          bottom: { xs: "-5%", sm: "10px", md: "10px", lg: "10px", xl: "10px" },
+          transform: "translate(-50%)",
+          width: "100%",
+          position: {
+            xs: "relative",
+            sm: "absolute",
+            md: "absolute",
+            lg: "absolute",
+            xl: "absolute",
+          },
+        }}
+      >
         <JumboCard
-          title="Lorem Ipsum"
+          title="Stay Updated With Our Newsletter!"
           contentWrapper
           contentSx={{ pt: 0 }}
         >
-          <Box display="flex" justifyContent="center" mt={2}>
-            <Button variant="contained" color="primary" fullWidth  sx={{ borderRadius: 10 }} onClick={handleOpen}>
-                Click
+          <Box display="flex" justifyContent="center">
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{
+                bgcolor: theme.palette.info.main,
+                "&:hover": {
+                  bgcolor: theme.palette.primary.main, // Change this to your desired hover color
+                },
+                borderRadius: 10,
+              }}
+              onClick={handleOpen}
+            >
+              Subscribe
             </Button>
-          </Box> 
+          </Box>
         </JumboCard>
       </Box>
       <Modal
@@ -85,28 +103,33 @@ function JumboNavbarProvider({
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
         sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-          <Box sx={{
+        <Box
+          sx={{
             width: {
-              xs: '90%',
-              sm: '75%',
-              md: '50%',
-              lg: '40%',
-              xl: '30%',
+              xs: "90%",
+              sm: "75%",
+              md: "50%",
+              lg: "40%",
+              xl: "30%",
             },
             height: {
-              xs: '90%'
+              xs: "90%",
             },
-            maxHeight: '100%',
-            overflowY: 'auto',
-            maxWidth: '100%',
-          }}>
-            <NewsLetterSubscription title={items.widgets.title} subheader={items.widgets.subheader} /> 
-          </Box>
+            maxHeight: "100%",
+            overflowY: "auto",
+            maxWidth: "100%",
+          }}
+        >
+          <NewsLetterSubscription
+            title={items.widgets.title}
+            subheader={items.widgets.subheader}
+          />
+        </Box>
       </Modal>
     </JumboNavbarContext.Provider>
   );
@@ -148,5 +171,5 @@ JumboNavbarProvider.propTypes = {
   mini: PropTypes.bool,
   open: PropTypes.bool,
   theme: PropTypes.object,
-  groupBehaviour: PropTypes.oneOf(['collapsible', 'popover']),
+  groupBehaviour: PropTypes.oneOf(["collapsible", "popover"]),
 };
