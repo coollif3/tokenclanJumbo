7eb8c33131e6679c8864b40471a63d1479527152
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-10-04 15:34:09 +0800

    Add suspense

--- src/app/[lang]/(common)/blockchains/page.js
+++ src/app/[lang]/(common)/blockchains/page.js
@@ -1,14 +1,22 @@
-import BlockchainDataTable from "@app/_components/widgets/BlockchainTableListing/BlockchainDataTable";
+import { lazy, Suspense } from "react";
 import {
   getBlockchainMktOverviewChng,
   getBlockchainMktOverview,
   getBlockchains,
 } from "@app/_services/blockchain";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, CircularProgress } from "@mui/material";
 import { CONTAINER_MAX_WIDTH } from "@app/_config/layouts";
-import Chart from "@app/_components/charts/apex/Chart";
 import PercentChngCard from "@app/_components/metrics/PercentChngCard/PercentChngCard";
 import CurrentMarketCard from "@app/_components/widgets/CurrentMarketCard/CurrentMarketCard";
+
+const BlockchainDataTable = lazy(
+  () =>
+    import(
+      "@app/_components/widgets/BlockchainTableListing/BlockchainDataTable"
+    )
+);
+
+const Chart = lazy(() => import("@app/_components/charts/apex/Chart"));
 
 export const metadata = {
   title: "Blockchain Market Cap / Total Value Lock (TVL) Data",
@@ -16,61 +24,18 @@ export const metadata = {
     "Explore the latest trends and get insights into blockchains where crypto investors park their digital assets.",
 };
 
-async function DisplayBlockchainTable() {
-  const listingRows = await getBlockchains();
-  return <BlockchainDataTable rows={listingRows} />;
-}
-
-async function DisplayTVLChart() {
-  const chartSeries = await getBlockchainMktOverview();
-  const chartConfig = {
-    chartTitle: "Total Blockchain TVL",
-    tooltipSeries: "Total TVL",
-    yaxisTitle: "TVL in USD (Billion)",
-    yaxisFormatter: "BILLION_UNIT",
-    yaxisTooltipFormatterLabel: "DOLLAR",
-  };
-  return <Chart series={chartSeries} config={chartConfig} />;
-}
-
-async function DisplayBlockchainCards() {
-  const chngData = await getBlockchainMktOverviewChng();
-  return (
-    <>
-      <Grid item xs={12} sm={6} md={3}>
-        <CurrentMarketCard
-          subheader={"Today's TVL USD"}
-          value={chngData.all_total_usd}
-          prefixUnit="$"
-          roundedDigit={0}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <PercentChngCard
-          title={`24hr Change`}
-          value={parseFloat(chngData.one_day_chng)}
-          period={"day"}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <PercentChngCard
-          title={`7 Day Change`}
-          value={parseFloat(chngData.seven_day_chng)}
-          period={"week"}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <PercentChngCard
-          title={`30 Day Change`}
-          value={parseFloat(chngData.thirty_day_chng)}
-          period={"month"}
-        />
-      </Grid>
-    </>
-  );
-}
+const chartConfig = {
+  chartTitle: "Total Blockchain TVL",
+  tooltipSeries: "Total TVL",
+  yaxisTitle: "TVL in USD (Billion)",
+  yaxisFormatter: "BILLION_UNIT",
+  yaxisTooltipFormatterLabel: "DOLLAR",
+};
 
 const BlockchainsPage = async () => {
+  const chngData = await getBlockchainMktOverviewChng();
+  const listingRows = await getBlockchains();
+  const chartSeries = await getBlockchainMktOverview();
   return (
     <Container
       maxWidth={false}
@@ -87,12 +52,44 @@ const BlockchainsPage = async () => {
         <Grid item xs={12}>
           <Typography variant="h3">Blockchain TVL</Typography>
         </Grid>
-        <DisplayBlockchainCards />
-        <Grid item xs={12}>
-          <DisplayTVLChart />
+        <Grid item xs={12} sm={6} md={3}>
+          <CurrentMarketCard
+            subheader={"Today's TVL USD"}
+            value={chngData.all_total_usd}
+            prefixUnit="$"
+            roundedDigit={0}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PercentChngCard
+            title={`24hr Change`}
+            value={parseFloat(chngData.one_day_chng)}
+            period={"day"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PercentChngCard
+            title={`7 Day Change`}
+            value={parseFloat(chngData.seven_day_chng)}
+            period={"week"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PercentChngCard
+            title={`30 Day Change`}
+            value={parseFloat(chngData.thirty_day_chng)}
+            period={"month"}
+          />
         </Grid>
         <Grid item xs={12}>
-          <DisplayBlockchainTable />
+          <Suspense fallback={<CircularProgress />}>
+            <Chart series={chartSeries} config={chartConfig} />
+          </Suspense>
+        </Grid>
+        <Grid item xs={12}>
+          <Suspense fallback={<CircularProgress />}>
+            <BlockchainDataTable rows={listingRows} />
+          </Suspense>
         </Grid>
       </Grid>
     </Container>
