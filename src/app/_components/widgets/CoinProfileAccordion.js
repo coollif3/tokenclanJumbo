277e9124239873c7b4886b109849e60708dff086
charlieng <charliengcl@outlook.com>
2024-10-08 19:14:23 +0800

    updated coinprofile accordion component

--- src/app/_components/widgets/CoinProfileAccordion.js
+++ src/app/_components/widgets/CoinProfileAccordion.js
@@ -1,78 +1,151 @@
-import { getCoinProfileFor } from "@app/_services/coin";
 import { splitIntoParagraphs } from "@app/_utilities/helpers";
-import { Grid, Typography, Link, Card, CardContent } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Link,
+  Card,
+  CardContent,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails
+} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import {
+  getCommonCoinProfileFor,
+} from "@app/_services/coin";
 
 // accordion component function is used to display the Coin Profile details
 export default async function CoinProfilePage({ slug }) {
-  const coinProfile = await getCoinProfileFor(slug);
-  // console.log("coinProfile: ", coinProfile);
+  const coinProfile = await getCommonCoinProfileFor(slug);
+  // console.log("slug: ", slug);
+  // console.log("commomCoinProfile: ", coinProfile);
+
   // mapping of the key names to display names
   const keyNameMapping = {
-    symbol: "Symbol",
-    coin_profile_name: "Coin Name",
-    description: `About ${coinProfile.coin_profile_name}`,
-    homepage: "Homepage URL",
-    subreddit_url: "Reddit",
+    dcp_symbol: "Gas Coin Symbol",
+    dcp_name: "Blockchain",
+    dcp_description: `About ${coinProfile.dcp_name} Gas Coin`,
+    dcp_homepage: "Homepage URL",
+    dcp_subreddit_url: "Reddit",
+    ecp_symbol: "Coin Symbol",
+    ecp_exchng_name: "Exchange",
+    ecp_description: `About ${coinProfile.ecp_name}`,
+    ecp_homepage: "Homepage URL",
+    ecp_subreddit_url: "Reddit",
+  };
+
+  // Function to render profile data to be displayed
+  const renderProfileData = (profileType) => {
+    return Object.keys(coinProfile).map((key) => {
+      // Skip keys that do not match the displayProfile value to be displayed
+      if (profileType === 'ecp' && key.startsWith('dcp_')) return null;
+      if (profileType === 'dcp' && key.startsWith('ecp_')) return null;
+
+      // Skip dcp_slug and ecp_slug display
+      if (key === 'dcp_slug' || key === 'ecp_slug' || key === 'ecp_name') return null;
+
+      let displayKey = keyNameMapping[key] || key;
+      let value = coinProfile[key] || "N.A";
+
+      return (
+        <Grid
+          item
+          xs={12}
+          sm={(key === 'dcp_description' || key === 'ecp_description') && value !== "N.A" ? 12 : 6}
+          md={(key === 'dcp_description' || key === 'ecp_description') && value !== "N.A" ? 12 : 6}
+          lg={(key === 'dcp_description' || key === 'ecp_description') && value !== "N.A" ? 12 : 6}
+          key={key}
+        >
+          <Card elevation={3} sx={{ border: "1px solid #ddd", p: 1, mb: 1 }}>
+            <CardContent>
+              <Typography variant="h6">{displayKey}</Typography>
+              {(key === 'dcp_description' || key === 'ecp_description') && value !== "N.A" ? (
+                splitIntoParagraphs(value).map((paragraph, index) => (
+                  <Typography variant="body1" paragraph key={index}>
+                    {paragraph}
+                  </Typography>
+                ))
+              ) : (
+                <Typography variant="body1">
+                  {["dcp_homepage", "dcp_subreddit_url", "ecp_homepage", "ecp_subreddit_url"].includes(key) && value !== "N.A" ? (
+                    <Link href={value} target="_blank" rel="noopener noreferrer">
+                      {value}
+                    </Link>
+                  ) : (
+                    value
+                  )}
+                </Typography>
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
+      );
+    });
   };
 
   return (
     <>
       <Grid container spacing={2}>
-        {Object.keys(coinProfile).map((key) => {
-          let displayKey = keyNameMapping[key] || key;
-          let value = coinProfile[key] || "N.A";
-
-          return (
-            <Grid
-              item
-              xs={12}
-              sm={
-                displayKey === `About ${coinProfile.coin_profile_name}` &&
-                value !== "N.A"
-                  ? 12
-                  : 6
-              }
-              md={
-                displayKey === `About ${coinProfile.coin_profile_name}` &&
-                value !== "N.A"
-                  ? 12
-                  : 4
-              }
-              key={key}
+        {coinProfile.dcp_slug !== null && coinProfile.ecp_slug === null && (
+          <Accordion
+            elevation={0}
+            sx={{ boxShadow: "none", mb: 3, border: "1px solid #ddd" }}
+          >
+            <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+              <Typography variant="h5">Blockchain Gas Coin Profile</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container spacing={2}>
+                {renderProfileData('dcp')}
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        )}
+        {coinProfile.dcp_slug === null && coinProfile.ecp_slug !== null && (
+          <Accordion
+            elevation={0}
+            sx={{ boxShadow: "none", mb: 3, border: "1px solid #ddd" }}
+          >
+            <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+              <Typography variant="h5">Exchange Coin Profile</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container spacing={2}>
+                {renderProfileData('ecp')}
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        )}
+        {coinProfile.dcp_slug !== null && coinProfile.ecp_slug !== null && (
+          <>
+            <Accordion
+              elevation={0}
+              sx={{ boxShadow: "none", mb: 3, border: "1px solid #ddd" }}
             >
-              <Card
-                elevation={3}
-                sx={{ border: "1px solid #ddd", p: 1, mb: 1 }}
-              >
-                <CardContent>
-                  <Typography variant="h6">{displayKey}</Typography>
-                  {key === "description" && value !== "N.A" ? (
-                    splitIntoParagraphs(value).map((paragraph, index) => (
-                      <Typography variant="body1" paragraph key={index}>
-                        {paragraph}
-                      </Typography>
-                    ))
-                  ) : (
-                    <Typography variant="body1">
-                      {["homepage", "subreddit_url"].includes(key) &&
-                      value !== "N.A" ? (
-                        <Link
-                          href={value}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {value}
-                        </Link>
-                      ) : (
-                        value
-                      )}
-                    </Typography>
-                  )}
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
+              <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+                <Typography variant="h5">Blockchain Gas Coin Profile</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Grid container spacing={2}>
+                  {renderProfileData('dcp')}
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              elevation={0}
+              sx={{ boxShadow: "none", mb: 3, border: "1px solid #ddd" }}
+            >
+              <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+                <Typography variant="h5">Exchange Coin Profile</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Grid container spacing={2}>
+                  {renderProfileData('ecp')}
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
+          </>
+        )}
       </Grid>
     </>
   );
