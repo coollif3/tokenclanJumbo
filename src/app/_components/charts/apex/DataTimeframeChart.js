5907149a3501d8bfe352ddf35d2e55aa5918a0f5
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2024-11-02 14:18:54 +0800

    Update DataTimeframeChart.js
    Remove unused codes daily weekly monthly chart

--- src/app/_components/charts/apex/DataTimeframeChart.js
+++ src/app/_components/charts/apex/DataTimeframeChart.js
@@ -1,12 +1,5 @@
 "use client";
-import {
-  Grid,
-  ButtonGroup,
-  Button,
-  Box,
-  Typography,
-  colors,
-} from "@mui/material";
+import { Grid, ButtonGroup, Button, Box, Typography } from "@mui/material";
 import Chart from "@app/_components/charts/apex/Chart";
 import { useEffect, useState } from "react";
 import { useJumboTheme } from "@jumbo/components/JumboTheme/hooks";
@@ -59,71 +52,13 @@ export default function DataTimeframeChart({
     fetchData();
   }, [slug, timeframe, dataFunc, compareToSlug, chartType]);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     let tvlData;
-  //     switch (chartType) {
-  //       case "daily":
-  //         tvlData = await getBlockchainTvlForSlug(slug, 30);
-  //         setTvlData(tvlData);
-  //         break;
-  //       case "weekly":
-  //         tvlData = await getBlockchainTvlForSlugWeek(slug, 12);
-  //         setTvlData(tvlData);
-  //         break;
-
-  //       case "monthly":
-  //         tvlData = await getBlockchainTvlForSlugMonth(slug, 6);
-  //         setTvlData(tvlData);
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //   }
-
-  //   fetchData();
-  // }, [slug, chartType]);
-
-  // const handleChartTypeChange = (chartType) => {
-  //   setChartType(chartType);
-  //   setTimeframe(30);
-  // };
-
   const handleTimeframeChange = (newTimeframe) => {
     setTimeframe(newTimeframe);
-    // setChartType("daily");
   };
 
   return (
     <Grid container spacing={3.75} sx={{ mb: 3 }}>
       <Grid item xs={12}>
-        {/* <Box sx={{ display: "flex", alignItems: "center" }}>
-            <ButtonGroup
-              variant="outlined"
-              size="small"
-              aria-label="outlined primary button group"
-              color="primary"
-            >
-              <Button
-                variant={chartType === "daily" ? "contained" : "outlined"}
-                onClick={() => handleChartTypeChange("daily")}
-              >
-                Daily
-              </Button>
-              <Button
-                variant={chartType === "weekly" ? "contained" : "outlined"}
-                onClick={() => handleChartTypeChange("weekly")}
-              >
-                Weekly
-              </Button>
-              <Button
-                variant={chartType === "monthly" ? "contained" : "outlined"}
-                onClick={() => handleChartTypeChange("monthly")}
-              >
-                Monthly
-              </Button>
-            </ButtonGroup>
-          </Box> */}
         <Box
           sx={{
             display: "flex",
