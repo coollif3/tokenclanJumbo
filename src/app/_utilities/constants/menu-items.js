d89c282759a46042b6d3c370f68e0f7278941aed
Parth Detroja <parth.detroja@aliansoftware.com>
2024-12-25 10:31:31 +0530

    Solve jumbo file bugs
    Solve jumbo file bugs

--- src/app/_utilities/constants/menu-items.js
+++ src/app/_utilities/constants/menu-items.js
@@ -2,17 +2,60 @@ import { getDictionary } from '@app/[lang]/dictionaries';
 
 export async function getMenus(locale) {
   const dictionary = await getDictionary(locale);
-  const { sidebar } = dictionary;
-  return [
+  const { sidebar, widgets } = dictionary;
+
+  const menuItems = [
     {
-      label: sidebar.menu.sample,
+      label: sidebar.menu.chart,
       children: [
         {
-          path: `/${locale}`,
-          label: sidebar.menuItem.sample,
-          icon: 'sample',
+          path: `/${locale}/blockchains`,
+          label: sidebar.menuItem.blockchains,
+          icon: "blockchains",
+        },
+        {
+          path: `/${locale}/exchanges`,
+          label: sidebar.menuItem.exchanges,
+          icon: "exchanges",
+        },
+        {
+          path: `/${locale}/coins`,
+          label: sidebar.menuItem.coins,
+          icon: "coins",
+        },
+      ],
+    },
+    {
+      label: sidebar.menu.main,
+      children: [
+        {
+          path: `/${locale}/tvev`,
+          label: sidebar.menuItem.tvevRatio,
+          icon: "tvevRatio",
+        },
+        {
+          path: `/${locale}/disclaimer`,
+          label: sidebar.menuItem.disclaimer,
+          icon: "disclaimer",
+        },
+        {
+          path: `/${locale}/privacy`,
+          label: sidebar.menuItem.privacy,
+          icon: "privacy",
+        },
+        {
+          path: `/${locale}/terms`,
+          label: sidebar.menuItem.terms,
+          icon: "terms",
+        },
+        {
+          path: `https://support.tokenclan.io`,
+          label: sidebar.menuItem.contact,
+          icon: "contact",
         },
       ],
     },
   ];
+
+  return { menuItems, widgets };
 }
