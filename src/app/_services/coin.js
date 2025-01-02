eec2958669960b7130e6ede4054d01cdb837e354
charlieng <charliengcl@outlook.com>
2025-01-03 00:51:19 +0800

    added commenting for newly added cache functions

--- src/app/_services/coin.js
+++ src/app/_services/coin.js
@@ -227,7 +227,7 @@ export const getCoinSlugUsingBlkChainSlug = async (slug) => {
   return await getData(slug);
 }
 
-// **need to create standalone view table for coin price chng data and alter the query for optimisation
+// Get coin price change latest data for a given slug
 export const getCoinPriceChngForSlug = async (slug) => {
   const getData = nextCache(
     cache(async (slug) => {
@@ -249,7 +249,7 @@ export const getCoinPriceChngForSlug = async (slug) => {
   return await getData(slug);
 };
 
-// **need to create standalone view table for coin price data and alter the query for optimisation
+// Get coin price data for a given slug
 export const getCoinPriceForSlug = async (slug, period) => {
   const getData = nextCache(
     cache(async (slug, period) => {
