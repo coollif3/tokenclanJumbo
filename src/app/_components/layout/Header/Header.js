752655ad277ff906797b3035ed503d004cf9263e
coollif3 <86946986+coollif3@users.noreply.github.com>
2025-09-14 23:22:02 +0800

    Fix client component errors by adding 'use client' directives

--- src/app/_components/layout/Header/Header.js
+++ src/app/_components/layout/Header/Header.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { ThemeModeOption } from "./components/ThemeModeOptions";
 import { DropDownPopover } from "@app/_components/popovers/DropDownPopover";
 import { Button } from '@mui/material';
