f9ed098833e682d315b32bfe9f738f0f81e37388
coollif3 <86946986+coollif3@users.noreply.github.com>
2025-09-14 20:37:59 +0800

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  transpilePackages: [
-    '@mui/material',
-    '@mui/icons-material',
-    '@mui/lab',
-    '@emotion/react',
-    '@emotion/styled',
-    '@mui/material-nextjs'
-  ],
   env: {
     REACT_APP_IMAGES_PATH: "/assets/images",
     NEXTAUTH_SECRET: "Wxh7ucB6n1ZpL2uSInvk/5Hl5WzgFFuPBhVfy0x6DG0U=",
