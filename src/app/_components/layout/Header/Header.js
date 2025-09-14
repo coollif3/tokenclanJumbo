a8d3947a9b122ad1ae28ff7e7edaa7be414c04ba
coollif3 <86946986+coollif3@users.noreply.github.com>
2025-09-14 23:32:55 +0800

    Fix Header component by adding 'use client' directive

--- src/app/_components/layout/Header/Header.js
+++ src/app/_components/layout/Header/Header.js
@@ -1,5 +1,7 @@
 'use client';
 
+'use client';
+
 import { ThemeModeOption } from "./components/ThemeModeOptions";
 import { DropDownPopover } from "@app/_components/popovers/DropDownPopover";
 import { Button } from '@mui/material';
